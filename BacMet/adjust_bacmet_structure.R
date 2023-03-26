2d061800894907d8d57334e2812ed0a4e682c369
Yen <qwertyuiop3910@gmail.com>
2023-03-27 01:02:36 +0800

    Create metal only database

--- BacMet/adjust_bacmet_structure.R
+++ BacMet/adjust_bacmet_structure.R
@@ -1,12 +1,32 @@
 library(tidyverse)
 
 # Modify structure file
-bacmet_official_structure <- read.table("D:/ARG_project/airborne_arg_uwtp/BacMet/BacMet_structure/BacMet_official_structure.txt", 
+bacmet_official_structure <- read.table("./BacMet_structure/BacMet_official_structure.txt", 
                               sep = "\t", header = TRUE)
-bacMet_structure <- read.table("D:/ARG_project/airborne_arg_uwtp/BacMet/BacMet_structure/BacMet_structure.txt", 
-                                       sep = "\t", header = TRUE)
-bacMet_structure <- bacMet_structure %>% 
-  separate(level1, c('BacMet_ID', 'Gene_name','Unknown','Accession','Complete_accession'), sep = "|")
+bacmet_curated_structure <- bacmet_official_structure %>% select(BacMet_ID, Gene_name, Compound)
+colnames(bacmet_curated_structure) <- c("gene","subtype","type")
+# Filter metal only
+metal_list <- c("Aluminium (Al)","Antimony (Sb)","Arsenic (As)","Bismuth (Bi)",
+                "Cadmium (Cd)","Chromium (Cr)","Cobalt (Co)","Copper (Cu)",
+                "Gallium (Ga)","Gold (Au)","Iron (Fe)","Lead (Pb)",
+                "Magnesium (Mg)","Manganese (Mn)","Mercury (Hg)","Molybdenum (Mo)",
+                "Nickel (Ni)","Selenium (Se)","Silver (Ag)","Tellurium (Te)",
+                "Vanadium (V)","Zinc (Zn)")
+
+metal_only_structure <- bacmet_curated_structure %>% 
+  filter(grepl("Aluminium|Antimony|Arsenic|Bismuth|
+               Cadmium|Chromium|Cobalt|Copper|
+               Gallium|Gold|Iron|Lead|
+               Magnesium|Manganese|Mercury|Molybdenum|
+               Nickel|Selenium|Silver|Tellurium|
+               Vanadium|Zinc",type)) # Filter to metal only dataframe
+
+metal_only_structure$type <- gsub(".*,.*" , "Multi-metal" , 
+                                  metal_only_structure$type)  # Add multi-metal tag
+
+# write.table(metal_only_structure, "./BacMet_structure/metal_only_structure.txt",
+#             sep = "\t", quote = FALSE, append = FALSE, row.names=  FALSE)
+
 
 
 
@@ -14,13 +34,10 @@ bacMet_structure <- bacMet_structure %>%
 
 # Modify fasta
 library(Biostrings)
-fasta <- readDNAStringSet("D:/ARG_project/airborne_arg_uwtp/BacMet/BacMet_structure/BacMet_experimentally_confirmed.fasta")
-
+library(stringr)
+fasta <- readDNAStringSet("./BacMet_structure/BacMet_experimentally_confirmed.fasta")
+names(fasta)
 # Modify the header using regular expressions
-newHeaders <- gsub("(>.*?\\|abeM).*", "", names(fasta))
-
-# assign the new headers to the object
-names(fasta) <- newHeaders
-
+names(fasta) <- str_extract_all(names(fasta),"^BAC....")
 # write out the modified fasta file
-writeXStringSet(fasta, "modified.fasta", format="fasta")
+writeXStringSet(fasta, "./BacMet_structure/BacMet_exp_modified.fasta", format="fasta")

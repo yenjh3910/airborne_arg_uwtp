c9531465ab1955ebd6caf223e77df1cd4caecd21
yenjh3910 <82366605+yenjh3910@users.noreply.github.com>
2023-03-28 02:08:56 +0800

    add humann3.sh

---
+++ shell_script/humann3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for i in $(<~/sample_list/clean_read_list)
+do
+
+cat ~/clean_read/${i}_1.fastq.gz  ~/clean_read/${i}_2.fastq.gz > ~/clean_read/${i}.fastq.gz
+
+# Run humann3
+humann -i ~/clean_read/${i}.fastq.gz -o ~/humann3 --input-format fastq.gz --threads 16
+
+rm ~/clean_read/${i}.fastq.gz
+
+done
